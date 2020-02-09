a0f06ba16567e0e258f78fbd5b384dd83958f596
zed <zed@mail.ru>
2020-02-09 21:51:59 +0300

    added cart

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from ..shop.models import Product
+from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -26,3 +26,7 @@ def cart_remove(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect("cart:cart_detail")
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, "cart/detail.html", {"cart":cart})
